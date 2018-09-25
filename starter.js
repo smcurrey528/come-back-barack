ca8696a4bad93d6e3d545b21be09722737a72f18
Sarah Currey <curreysarah28@gmail.com>
2018-09-25 18:26:48 -0400

    new files and changes

--- starter.js
+++ starter.js
@@ -2,6 +2,10 @@
 //jump over maga hats while trolls are chasing you
 
 //any pop-ups, about narractive?
+
+//Questions about collision into the hats
+//Question about having the game in an container vs the body/window of the browser
+//Questions about local storage, collecting the users' name
 window.onload = function () {
   const body = document.body;
  //creating a div with a class ".obama" to add Barack to the screen
