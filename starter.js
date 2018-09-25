f4b68f3ea471db3b974366d440717107f9a13cbe
Sarah Currey <curreysarah28@gmail.com>
2018-09-24 21:04:57 -0400

    starter css and html

---
+++ starter.js
@@ -0,0 +1,9 @@
+//you caught a liberal snowflake, great job!
+//jump over maga hats while trolls are chasing you
+
+//any pop-ups, about narractive?
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
