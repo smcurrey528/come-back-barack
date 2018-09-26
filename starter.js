05717e71e44075f59799f426f9b40a1cb007eac7
Sarah Currey <curreysarah28@gmail.com>
2018-09-25 21:38:38 -0400

    moving randomly elements

--- starter.js
+++ starter.js
@@ -21,25 +21,14 @@ return obama
  }
 createObama();
 
-// Creating a new function to make it look like Obama is moving to the left
-// using a toggle class to toggle to a new sprite image and using setInterval to toggle class
-// NOT WORKING YET
-function goLeft () {
-  setInterval(function() {
-  //   let obama = document.createElement('div');
-    obama.classList.toggle("left");
-  }, 300)
-  console.log('obama go left!');
-}
-goLeft();
 
 // create new element that will be the Liberal Snowflake for Obama to catch
 //set new element to class of snowflake
 function makeLiberalSnowflake() {
-let snowflake = document.createElement('div');
 snowflake.classList.add('snowflake');
 body.appendChild(snowflake);
 return snowflake
+
  }
 makeLiberalSnowflake();
 
@@ -47,7 +36,6 @@ makeLiberalSnowflake();
 //Using math.random() * window.innerHeight
 //NOT WORKING YET
 function moveSnowflake () {
- snowflake = document.createElement('div');
   snowflake.style.top = Math.random() * innerHeight +'px';
   snowflake.style.left = Math.random() * innerWidth + 'px';
 }
@@ -56,7 +44,6 @@ moveSnowflake();
 //create new element for the MAGA hat that will also fall from Sky and Obama needs to avoid
 //assign hat to the class of hat
 function stupidRedHat () {
-  let hat = document.createElement('div');
   hat.classList.add('hat');
   body.appendChild(hat);
   return hat
@@ -72,6 +59,13 @@ function moveHat () {
 moveHat();
 
 //function to move the hat and snowflake elements every second
+function flyingElements () {
+  setInterval(function() {
+  moveHat();
+  moveSnowflake();
+}, 1000)
+}
+flyingElements();
 
  };
 
