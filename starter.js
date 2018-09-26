8a6958003390cd51e7c0d223352c9837717ed565
Sarah Currey <curreysarah28@gmail.com>
2018-09-26 10:42:05 -0400

    js revisions. struggling with for loop

--- starter.js
+++ starter.js
@@ -1,76 +1,110 @@
-//you caught a liberal snowflake, great job!
-//jump over maga hats while trolls are chasing you
+// //you caught a liberal snowflake, great job!
+// //jump over maga hats while trolls are chasing you
 
-//any pop-ups, about narractive?
+// //any pop-ups, about narractive?
 
-//Questions about collision into the hats
-//Question about having the game in an container vs the body/window of the browser
-//Questions about local storage, collecting the users' name
+// //Questions about collision into the hats
+// //Question about having the game in an container vs the body/window of the browser
+// //Questions about local storage, collecting the users' name
 window.onload = function () {
   const body = document.body;
   let obama = document.createElement('div');
   let snowflake = document.createElement('div');
   let hat = document.createElement('div');
 
- // //creating a div with a class ".obama" to add Barack to the screen
+//  // //creating a div with a class ".obama" to add Barack to the screen
 function createObama() {
-let obama = document.createElement('div');
-obama.classList.add('obama');
-body.appendChild(obama);
-return obama
+  let obama = document.createElement('div');
+  obama.classList.add('obama');
+  body.appendChild(obama);
+  return obama
  }
 createObama();
 
-
-// create new element that will be the Liberal Snowflake for Obama to catch
-//set new element to class of snowflake
+// // create new element that will be the Liberal Snowflake for Obama to catch
+// //set new element to class of snowflake
 function makeLiberalSnowflake() {
-snowflake.classList.add('snowflake');
-body.appendChild(snowflake);
-return snowflake
+  snowflake.classList.add('snowflake');
+  body.appendChild(snowflake);
+  return snowflake
 
  }
-// makeLiberalSnowflake();
+makeLiberalSnowflake();
 
-//create a function to move the snowflake so they are falling down the page using CSS styles.
-//Using math.random() * window.innerHeight
-//NOT WORKING YET
+// //create a function to move the snowflake so they are falling down the page using CSS styles.
+// //Using math.random() * window.innerHeight
+
 function moveSnowflake () {
   snowflake.style.top = Math.random() * innerHeight +'px';
-  snowflake.style.left = Math.random() * innerWidth + 'px';
+  // snowflake.style.left = Math.random() * innerWidth + 'px';
 }
 moveSnowflake();
 
-//create new element for the MAGA hat that will also fall from Sky and Obama needs to avoid
-//assign hat to the class of hat
+// //create new element for the MAGA hat that will also fall from Sky and Obama needs to avoid
+// //assign hat to the class of hat
 function stupidRedHat () {
   hat.classList.add('hat');
   body.appendChild(hat);
   return hat
 }
-// stupidRedHat();
+stupidRedHat();
 
-//create a function to move the hat so they are falling down the page using CSS styles.
-//Using math.random() * window.innerHeight
+// //create a function to move the hat so they are falling down the page using CSS styles.
+// //Using math.random() * window.innerHeight
 function moveHat () {
   hat.style.top = (Math.random() * innerHeight) + 'px';
-  hat.style.left =(Math.random() * innerWidth) + 'px';
+  // hat.style.left =(Math.random() * innerWidth) + 'px';
 }
 moveHat();
 
-//function to move the hat and snowflake elements every second
+// //function to move the hat and snowflake elements every second
 function flyingElements () {
   setInterval(function() {
   moveHat();
   moveSnowflake();
-}, 500)
+}, 800)
 }
 flyingElements();
 
-//creating a lot of red hats and snowflakes in the screen with for loop function
-for (i=0; i < 10; i++) {
+// //creating a lot of red hats and snowflakes in the screen with for loop function
+for (let i=0; i < 5; i++) {
   makeLiberalSnowflake();
-  stupidRedHat();
+  // stupidRedHat();
+  console.log("testing");
+ }
  };
-}
+
+ // create an event listener function that will remove the snowflake once it has been clicked
+ body.addEventListener('click', caughtSnowflake)
+
+ function caughtSnowflake (event) {
+  if (event.target.classList[0] === 'snowflake') {
+    event.target.classList.add('energize')
+  }
+ }
+ caughtSnowflake();
+
+// window.onload = function () {
+//   const body = document.body;
+//   let obama = document.createElement('div');
+//   let snowflake = document.createElement('div');
+//   let hat = document.createElement('div');
+
+// function makeSnowflake () {
+//   let snowflake = document.createElement('div');
+//   snowflake.classList.add('snowflake');
+//   body.appendChild(snowflake);
+
+//   function moveSnowflake() {
+//     snowflake.style.top = Math.random() * window.InnerHeight + 'px'
+//   };
+//   moveSnowflake();
+//   setInterval(moveSnowflake, 1000);
+
+//   for (let i= 0; i < 5; i++) {
+//     makeSnowflake();
+//   };
+// }
+// }
+
 
